d8d103ff01f6c7a14db795da745ee3c3b3bf4b1a
h72020104-tech <h72020104@gmail.com>
2026-02-09 20:31:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,18 @@
-const CACHE_NAME = 'mnasati-app-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  'https://e.top4top.io/p_3692gqofg1.png'
-];
-
-// تثبيت الـ Service Worker
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open("pwa-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html"
+      ]);
+    })
   );
 });
 
-// تفعيل واسترجاع الملفات (Offline support)
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
