92381019d269b53d7688913a1bc29393e07b1a11
h72020104-tech <h72020104@gmail.com>
2026-02-11 15:58:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,27 @@
-self.addEventListener("install", function(event) {
-  console.log("Service Worker Installed");
+const CACHE_NAME = 'haider-platform-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
